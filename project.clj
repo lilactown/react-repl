fe65386e678ef776c8b501737975b63df9147cc2
Will Acton <will@willacton.com>
2021-08-27 09:33:26 -0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject org.clojars.lilactown/react-repl "0.0.1"
   :description "Tools for interacting with a React app from a ClojureScript REPL"
   :url "https://github.com/lilactown/react-repl"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v20.html"}
+  :license {:name "Apache2 License"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :source-paths ["src"]
   :dependencies [[cljs-bean "1.5.0"]]
   :deploy-repositories [["snapshots" {:sign-releases false :url "https://clojars.org"}]])
